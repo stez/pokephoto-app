dbbedf85ec8b0cc702198427556db5ce9368534e
Stefano Zanotti <stez78@gmail.com>
2022-08-11 00:17:12 +0200

    removes login button on app bar

--- pages/components/appbar.tsx
+++ pages/components/appbar.tsx
@@ -11,9 +11,6 @@ export default function AppBar() {
                 <span className="ml-4 self-center text-2xl font-extrabold whitespace-nowrap text-white"> PhotoApp</span>
             </div>
             </Link>
-            <div className="flex md:order-2">
-                <button type="button" className=" text-pokemon-dark  bg-pokemon-secondary hover:bg-pokemon-secondarydark hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">Login</button>
-            </div>
         </div>
     </nav>
   )
